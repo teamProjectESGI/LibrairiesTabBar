37f8f30d30225ec0ee07937c1507bbc738587f75
Thibaut <tibdev78@gmail.com>
2019-01-08 23:18:51 +0100

    first commit

--- AdaptiveTabBar/AdaptiveTabBar.swift
+++ AdaptiveTabBar/AdaptiveTabBar.swift
@@ -1,11 +1,3 @@
-//
-//  AdaptiveTabBar.swift
-//  AdaptiveTabBar
-//
-//  Created by TDSV on 08/01/2019.
-//  Copyright © 2019 TDSV. All rights reserved.
-//
-
 class AdaptiveTabBar {
 
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+#Adaptive Tab Bar in Swift
