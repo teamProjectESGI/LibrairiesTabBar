591e3acabe2ab202c4bcb7c58a95e08c2fa8d4de
tibdev78 <tibde78@gmail.com>
2019-01-26 18:53:44 +0100

    test Stackview

--- RectTest/Rect/RectViewController.swift
+++ RectTest/Rect/RectViewController.swift
@@ -17,29 +17,45 @@ class RectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let vLayout = VerticalLayout(width: 50 * self.screeWidth / 100, height: self.screenHeight )
+        let vLayout = VerticalLayout(width: 40 * self.screeWidth / 100, height: self.screenHeight )
         vLayout.backgroundColor = UIColor.cyan
         view.addSubview(vLayout)
-
-        let image1 = UIImage(named: "notification")
+        
+        let image1 = UIImage(named: "home")
         let button1 = UIButton()
         let test1 = image1?.resizedImage(newSize: CGSize(width: 30, height: 30))
-        button1.frame = CGRect(x: 0, y: 40 * screenHeight / 100, width: 150, height: 100)
-        button1.setTitle("Home", for: [])
-        button1.setTitleColor(.black, for: [])
-        button1.titleEdgeInsets = UIEdgeInsets(top: 0, left: 60, bottom: 0, right: 0)
+        button1.frame = CGRect(x: 0, y: 0, width: 110, height: 100)
+        button1.setTitle("Home", for: .normal)
+        button1.setTitleColor(.black, for: .normal)
+        button1.titleEdgeInsets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 0)
         button1.setImage(test1, for: .normal)
         button1.addTarget(self, action:#selector(RectViewController.imageButtonTapped(_:)), for: .touchUpInside)
-        vLayout.addSubview(button1)
 
         let image2 = UIImage(named: "notification")
         let test2 = image2?.resizedImage(newSize: CGSize(width: 30, height: 30))
-        let button2 = UIButton(frame: CGRect(x: 0, y: 20 * screenHeight / 100, width: 200, height: 100))
+        let button2 = UIButton(frame: CGRect(x: 0, y: 0, width: 160, height: 100))
         button2.setTitle("Notification", for: [])
         button2.setTitleColor(.black, for: [])
-        button2.titleEdgeInsets = UIEdgeInsets(top: 0, left: 60, bottom: 0, right: 0)
+        button2.titleEdgeInsets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 0)
         button2.setImage(test2, for: .normal)
-        vLayout.addSubview(button2)
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        redView.addSubview(button1)
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        blueView.addSubview(button2)
+        
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        vLayout.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.widthAnchor.constraint(equalToConstant: 165).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        stackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+        
         
         
     }
