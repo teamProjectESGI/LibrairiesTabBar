52216c46ecf43dc5eceea0b881be130982ff2de0
tibdev78 <tibde78@gmail.com>
2019-02-03 11:08:44 +0100

    button more 1

--- RectTest/Rect/RectViewController.swift
+++ RectTest/Rect/RectViewController.swift
@@ -9,59 +9,112 @@
 import UIKit
 
 class RectViewController: UIViewController {
+    private var arrayView: [UIView] = []
+    private var tableView: UITableView!
     
-    @IBOutlet weak var tabBarSubview: viewLayout!
-    @IBOutlet weak var firstIcon: UIButton!
-    @IBOutlet weak var secondIcon: UIButton!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let vLayout = VerticalLayout(width: 40 * self.screeWidth / 100, height: self.screenHeight )
+        tableView = UITableView(frame: CGRect(x: 200, y: 300, width: 150, height: 300))
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        tableView.dataSource = self
+        //myTableView.delegate = self
+        tableView.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
+        tableView.isHidden = true
+        self.view.addSubview(tableView)
+        
+        let vLayout = VerticalLayout(width: 30 * self.screeWidth / 100, height: self.screenHeight )
         vLayout.backgroundColor = UIColor.cyan
         view.addSubview(vLayout)
-        
+
         let image1 = UIImage(named: "home")
-        let button1 = UIButton()
+        let button1 = UIButton(frame:  CGRect(x: 0, y: 0, width: 110, height: 100))
         let test1 = image1?.resizedImage(newSize: CGSize(width: 30, height: 30))
-        button1.frame = CGRect(x: 0, y: 0, width: 110, height: 100)
-        button1.setTitle("Home", for: .normal)
-        button1.setTitleColor(.black, for: .normal)
-        button1.titleEdgeInsets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 0)
         button1.setImage(test1, for: .normal)
-        button1.addTarget(self, action:#selector(RectViewController.imageButtonTapped(_:)), for: .touchUpInside)
 
         let image2 = UIImage(named: "notification")
         let test2 = image2?.resizedImage(newSize: CGSize(width: 30, height: 30))
-        let button2 = UIButton(frame: CGRect(x: 0, y: 0, width: 160, height: 100))
-        button2.setTitle("Notification", for: [])
-        button2.setTitleColor(.black, for: [])
-        button2.titleEdgeInsets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 0)
+        let button2 = UIButton(frame: CGRect(x: 0, y: 0, width: 110, height: 100))
         button2.setImage(test2, for: .normal)
         
-        let redView = UIView()
-        redView.backgroundColor = .red
-        redView.addSubview(button1)
+        let image3 = UIImage(named: "contact")
+        let test3 = image3?.resizedImage(newSize: CGSize(width: 30, height: 30))
+        let button3 = UIButton(frame: CGRect(x: 0, y: 0, width: 110, height: 100))
+        button3.setImage(test3, for: .normal)
         
-        let blueView = UIView()
-        blueView.backgroundColor = .blue
-        blueView.addSubview(button2)
+        let image4 = UIImage(named: "envelope")
+        let test4 = image4?.resizedImage(newSize: CGSize(width: 30, height: 30))
+        let button4 = UIButton(frame: CGRect(x: 0, y: 0, width: 110, height: 100))
+        button4.setImage(test4, for: .normal)
         
-        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        let image5 = UIImage(named: "time")
+        let test5 = image5?.resizedImage(newSize: CGSize(width: 30, height: 30))
+        let button5 = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 50))
+        button5.setImage(test5, for: .normal)
+        
+        let image6 = UIImage(named: "home")
+        let test6 = image6?.resizedImage(newSize: CGSize(width: 30, height: 30))
+        let button6 = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 50))
+        button6.setImage(test6, for: .normal)
+        
+        let imageMore = UIImage(named: "more")
+        let testImageMore = imageMore?.resizedImage(newSize: CGSize(width: 30, height: 30))
+        let buttonImageMore = UIButton(frame: CGRect(x: 0, y: 0, width: 110, height: 100))
+        buttonImageMore.addTarget(self, action:#selector(RectViewController.buttonmore(_:)), for: .touchUpInside)
+        buttonImageMore.setImage(testImageMore, for: .normal)
+
+        let view1 = UIView()
+        view1.backgroundColor = .red
+        view1.addSubview(button1)
+
+        let view2 = UIView()
+        view2.backgroundColor = .blue
+        view2.addSubview(button2)
+        
+        let view3 = UIView()
+        view3.backgroundColor = .brown
+        view3.addSubview(button3)
+        
+        let view4 = UIView()
+        view4.backgroundColor = .yellow
+        view4.addSubview(button4)
+        
+        let view5 = UIView()
+        view5.backgroundColor = .green
+        view5.addSubview(button5)
+        
+        let view6 = UIView()
+        view6.backgroundColor = .blue
+        view6.addSubview(button6)
+        
+        let viewImageMore = UIView()
+        viewImageMore.backgroundColor = .green
+        viewImageMore.addSubview(buttonImageMore)
+
+        let stackView = UIStackView(arrangedSubviews: [view1, view2, view3, view4, view5, view6])
         stackView.axis = .vertical
-        stackView.distribution = .fillEqually
+        stackView.distribution = .fillProportionally
         vLayout.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.widthAnchor.constraint(equalToConstant: 165).isActive = true
-        stackView.heightAnchor.constraint(equalToConstant: 400).isActive = true
-        stackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+        stackView.widthAnchor.constraint(equalToConstant: 125).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 820).isActive = true
+        stackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 80).isActive = true
+
+        if(stackView.arrangedSubviews.count > 5 ) {
+            view5.removeFromSuperview()
+            view6.removeFromSuperview()
+            stackView.addArrangedSubview(viewImageMore)
+            print(stackView.arrangedSubviews.count)
+        }
         
-        
+        self.arrayView.append(view5)
+        self.arrayView.append(view6)
+       
         
     }
     
-    @objc func imageButtonTapped(_ sender:UIButton!) {
-        
+    @objc func buttonmore(_ sender:UIButton!) {
+        tableView.isHidden = !tableView.isHidden
     }
 }
 
@@ -71,6 +124,11 @@ extension RectViewController {
     }
     public var screenHeight: CGFloat {
         return UIScreen.main.bounds.height
+    }
+    public func getButtonMore(stackView: UIStackView, view: [UIView]) {
+        if(stackView.arrangedSubviews.count > 5) {
+            
+        }
     }
 }
 
@@ -85,4 +143,25 @@ extension UIImage {
         UIGraphicsEndImageContext()
         return newImage
     }
+}
+
+extension RectViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayView.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        // check if there is a view at the indexPath
+        if indexPath.row < self.arrayView.count {
+            // there is a view, add it to the cells contentView
+            cell.contentView.addSubview(self.arrayView[indexPath.row])
+        } else {
+            print("no view at index")
+        }
+        cell.backgroundColor = UIColor.gray.withAlphaComponent(0)
+        return cell
+    }
+    
+    
 }
